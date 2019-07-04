d400500b2038cbc40b47ca90adf4e607632f719b
benibana2001 <benibana2001@gmail.com>
2019-07-04 16:33:33 +0900

    q03.2

--- q02.3/q02.3.go
+++ q02.3/q02.3.go
@@ -1,7 +1,44 @@
-package q02_3
+package main
 
-import "fmt"
+import (
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("World")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Set Header
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content_Type", "application/json")
+
+	source := map[string]string{
+		"Hello": "World",
+	}
+
+	file, err := os.Create("json.txt.gzip")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+
+
+	writerGzip := gzip.NewWriter(file)
+	writerGzip.Header.Name = "json.txt"
+	writerGzip.Close()
+	//result, _ := json.Marshal(&source)
+	//io.WriteString(writerGzip, string(result))
+
+	// セットしたio.Writer にエンコードと書き出しを行う
+	//encoder.Encode(source)
+	//io.WriteString(w, "http.Response Writer sample")
 }
--- q03.2/q03.2.go
+++ q03.2/q03.2.go
@@ -1,1 +1,24 @@
-package q03_2
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// ランダムな文字で埋められたファイルを作成
+	// ファイルサイズは 1024 byte とする
+
+	writer, err := os.Create("new.txt")
+	if err != nil {
+		fmt.Println("Failed to create new file")
+		os.Exit(1)
+	}
+
+	reader := rand.Reader
+	lReader := io.LimitReader(reader, 1024)
+
+	io.Copy(writer, lReader)
+}
