42ccaae8d8a8c59981bf3d6cf6cea865f8b39842
benibana2001 <benibana2001@gmail.com>
2019-07-04 19:36:14 +0900

    edit q03.3

--- q03.3/q03.3.go
+++ q03.3/q03.3.go
@@ -1,1 +1,20 @@
-package q03_3
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	file, _ := os.Create("new.zip")
+	zipWriter := zip.NewWriter(file)
+	defer zipWriter.Close()
+
+	writer, _ := zipWriter.Create("new.txt")
+	reader := strings.NewReader("Example of zipWriter\n")
+
+	io.Copy(writer, reader)
+}
